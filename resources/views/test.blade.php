7711e1a74bf3cc6791deb83d248c904bf767ec65
Simran <seemachauhan4836@gmail.com>
2020-08-07 16:46:58 +0530

    new file created and upadted

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -7,6 +7,8 @@
 </head>
 <body>
     <h1>Testing content......06 august</h1>
-   <h2>Ekarma Training Program. @2020</h2>
+  <p> What is Lorem Ipsum?
+Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+ Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</p>
 </body>
 </html>
